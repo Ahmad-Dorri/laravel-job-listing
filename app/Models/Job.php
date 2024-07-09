48e612dab8856b30222c4e0d31cad0c49cab75d8
Ahmad-Dorri <ahmaddorri110@gmail.com>
2024-07-09 08:40:01 +0330

    Define the relationship

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Job extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+
+    public function employer(): BelongsTo
+    {
+        return $this->belongsTo(Employer::class);
+    }
+
+    public function tags(): HasMany
+    {
+        return $this->hasMany(Tag::class);
+    }
 }
