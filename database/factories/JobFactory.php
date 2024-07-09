cb6484aaf7451bc6ec5f1938d68a3182cf28f45a
Ahmad-Dorri <ahmaddorri110@gmail.com>
2024-07-09 20:11:49 +0330

    Create factories

--- database/factories/JobFactory.php
+++ database/factories/JobFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Employer;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,13 @@ class JobFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'employer_id' => Employer::factory(),
+            'title' => $this->faker->jobTitle(),
+            'salary' => $this->faker->randomElement(['$50,000 USD', '$100,000 USD', '$150,000 USD', '$10,000 USD']),
+            'location' => $this->faker->address(),
+            'schedule' => $this->faker->randomElement(['Full Time', 'Part Time', 'Temporary']),
+            'url' => $this->faker->url(),
+            'featured' => false
         ];
     }
 }
